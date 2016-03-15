7842676f7c00462a0032c99a8fffbdf989a684b2
CarlosDiogo01 <carlos.sa01@gmail.com>
2016-03-15 18:04:33 +0000

    Finished project

--- src/run_macbookair.sh
+++ src/run_macbookair.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-max_threads=64
+max_threads=16
 max_sample=5
-num_threads=1
+numthreads=1
 
 a=2
 b=242
 n=12
-
 
 echo "Executing Ex1 for pth_hello..."
 
@@ -16,15 +15,15 @@ echo "NThreads,Average_Time_Per_Thread(ms),Time(ms)" >> CSV/pth_hello_output.csv
 echo "Sequential pth_hello..."
 for ((sample=1; sample <= $max_sample; sample++ ))
 do	
-	./pth_hello $num_threads >> CSV/pth_hello_output.csv
+	./pth_hello 1 >> CSV/pth_hello_output.csv
 done
 
 echo "Parallel pth_hello using threads...."
-for (( num_threads=2; num_threads <= $max_threads; num_threads+=2 ))
+for (( numthreads=2; numthreads <= $max_threads; numthreads+=2 ))
 do
 	for ((sample=1; sample <= $max_sample; sample++ ))
 	do	
-		./pth_hello $num_threads >> CSV/pth_hello_output.csv
+		./pth_hello $numthreads >> CSV/pth_hello_output.csv
 	done
 done
 
@@ -38,7 +37,7 @@ echo "MutualExclusion,NThreads,val_a,val_b,nRectangles/Sub-Intervals,Result,Time
 echo "MutualExclusion,NThreads,val_a,val_b,nRectangles/Sub-Intervals,Result,Time(ms)" >> CSV/trap_busywaiting_output.csv
 
 echo "Sequential MUTEX | SEMAPHORE | BUSY-WAITING..."
-for ((sample=1; sample <= $max_sample; sample++ ))
+for (( sample=1; sample <= $max_sample; sample++ ))
 do	
 	./trap_mutex 1 $a $b $n >> CSV/trap_mutex_output.csv
 	./trap_semaphore 1 $a $b $n >> CSV/trap_semaphore_output.csv
@@ -46,13 +45,13 @@ do
 done
 
 echo "Parallel MUTEX | SEMAPHORE | BUSY-WAITING...."
-for (( num_threads=2; num_threads <= $max_threads; num_threads+=2 ))
+for (( numthreads=2; numthreads <= $max_threads; numthreads+=2 ))
 do
 	for ((sample=1; sample <= $max_sample; sample++ ))
 	do	
-		./trap_mutex $num_threads $a $b $n >> CSV/trap_mutex_output.csv
-		./trap_semaphore $num_threads $a $b $n >> CSV/trap_semaphore_output.csv
-		./trap_busywaiting $num_threads $a $b $n >> CSV/trap_busywaiting_output.csv
+		./trap_mutex $numthreads $a $b $n >> CSV/trap_mutex_output.csv
+		./trap_semaphore $numthreads $a $b $n >> CSV/trap_semaphore_output.csv
+		./trap_busywaiting $numthreads $a $b $n >> CSV/trap_busywaiting_output.csv
 	done
 done
 echo "Done! All CSV's generated!...."
